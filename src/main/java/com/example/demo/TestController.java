65688f5b1f5f8db8ce4fb9eb3d5a9451219d6347
MIdWit0828 <kindj9897@gmail.com>
2024-05-20 14:14:16 +0900

    22

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,2 +1,13 @@
-package com.example.demo;public class TestController {
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "hello world!22";
+    }
 }
